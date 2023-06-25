4729b169c21214c95d46e2255154d120a5610835
Thabo1995 <mailathabo1@gmail.com>
2023-06-25 18:25:37 +0200

    --newcontent

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,7 +1,7 @@
 import resume from '../assets/pdf/resume.pdf'
 import profileImage from '../assets/pdf/profileimage.jpg'
 export const headerData = {
-    name: 'Bokang alvina ntsoane',
+    name: 'Bokang Alvina Ntsoane',
     title: "Human Resource Manager",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
     image: profileImage,
