6a40ad7c5186d0fad0cbbba93aa4187a2786fdf7
Thabo1995 <35290649+Thabo1995@users.noreply.github.com>
2023-06-28 16:19:01 +0200

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -8,9 +8,9 @@ export const experienceData = [
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: 'Feb2018',
+        company: 'Tshwane University of Technology',
+        jobtitle: 'Human Resources trainee',
+        startYear: 'Feb 2018',
         endYear: 'Mar 2019'
     },
 
